ebd4693f247ea9d494abcf952e44cb65e7cc3c21
dmarler <daltonmarler@outlook.com>
2023-12-12 18:33:39 -0600

    Finished Day 2 part 1

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,21 +1,63 @@
+use std::collections::HashMap;
+use std::cmp::max;
 
-pub fn part_1(input: String) -> u32 {
+pub fn part_1(input: String) -> i32 {
     println!("Day 2, Part 1");
 
-    let mut total: u32 = 0;
+    let mut total: i32 = 0;
 
     for line in input.lines() {
         let parts: Vec<&str> = line
             .split(":")
             .collect();
 
-        let game: i8 = parts[0]
+        let game: i32 = parts[0]
             .split(" ")
-            .collect::<Vec<_>>()[1]
+            .collect::<Vec<&str>>()[1]
             .parse()
             .expect("Failed to convert game number");
 
-        println!("{game}");
+        let rounds = parts[1]
+            .split(";")
+            .collect::<Vec<&str>>();
+
+        let mut scores: HashMap<&str, i32> = HashMap::new();
+
+        // For each round:
+        for round in rounds {
+            let colors = round
+                .split(",")
+                .collect::<Vec<&str>>();
+
+            for color in colors {
+                let values: Vec<&str> = color
+                    .trim()
+                    .split(" ")
+                    .collect::<Vec<&str>>();
+
+                let current: i32 = *scores.entry(values[1]).or_default();
+
+                scores.insert(
+                    values[1],
+                    max(values[0]
+                        .parse::<i32>()
+                        .expect("Failed to parse dice."),
+                        current));
+            }
+
+        }
+
+        // Check scores
+        if
+            *scores.entry("red").or_default() <= 12 &&
+            *scores.entry("green").or_default() <= 13 &&
+            *scores.entry("blue").or_default() <= 14
+            {
+            total += game;
+        }
+
+        scores.clear();
+
     }
 
     return total;
